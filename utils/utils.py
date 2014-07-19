d3459d3226a6f7a83b226202cefafdca74f4a27c
Tom Gurion <nagasaki45@gmail.com>
2014-07-19 19:45:30 +0300

    Moving general utility fuction from video.py to utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -2,8 +2,11 @@
 Some utility functions.
 '''
 
-from scipy import stats
+from collections import Iterable
+
+import pandas as pd
 import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 
 GROUP_A = [4, 6, 7, 8, 9, 10, 11, 13, 14, 15, 16]
@@ -14,11 +17,33 @@ def to_groups(l):
     return l.loc[GROUP_A], l.loc[GROUP_B]
 
 
-def independent_one_sided_ttest_summary(a, b, **kwargs):
-    '''
-    Print statistics about ttests between a and b.
-    '''
+def flatten(items, ignored_types=(str, bytes)):
+    '''From the cookbook, p.135.'''
+    for x in items:
+        if isinstance(x, Iterable) and not isinstance(x, ignored_types):
+            yield from flatten(x, ignored_types)
+        else:
+            yield x
 
+
+def list_to_chunks(l, size):
+    return [l[i:i + size] for i in range(0, len(l), size)]
+
+
+def divide_apply_unpack(data, function, dimension=2, args=(), index=None):
+    '''Divide x into list of n-dimensional vectors, apply the function
+    on each of them, unpack (flatten) and return.
+    '''
+    chunks = list_to_chunks(data, dimension)
+    applied = [function(x, *args) for x in chunks]
+    flat = list(flatten(applied))
+    if len(flat) == len(data):
+        return flat
+    return pd.Series(flat, index=index)
+
+
+def independent_one_sided_ttest_summary(a, b, **kwargs):
+    '''Print statistics about ttests between a and b.'''
     # two-sided result everywhere
     # more info regarding conversion to one-sided here:
     # http://stackoverflow.com/a/15984310/1224456
@@ -39,3 +64,8 @@ def independent_one_sided_ttest_summary(a, b, **kwargs):
     plt.xlim((-0.5, 1.5))
     for method, value in kwargs.items():
         getattr(plt, method)(value)
+
+
+# TESTS!
+# list_to_chunks
+assert list_to_chunks([1, 2, 3, 4], 2) == [[1, 2], [3, 4]]
--- utils/video.py
+++ utils/video.py
@@ -7,10 +7,6 @@ def append_one_and_stack(vecs):
     return np.vstack([m, np.ones(shape=[1, m.shape[1]])])
 
 
-def list_to_chunks(l, size):
-    return [l[i:i + size] for i in range(0, len(l), size)]
-
-
 def solve_affine_transformation(Xs, Ys):
     '''Returns the affine transformation as A and b vector.
     Xs and Ys should be equal length lists of X and Y vectors of the same
@@ -35,23 +31,11 @@ def transform(x, A, b):
     return np.dot(A, x) + b
 
 
-def transform_all(x, A, b, dimension=2):
-    '''Divide x into list of n-dimensional vector, apply the affine
-    transformation for each of them, concatenate and return.
-    '''
-    Xs = list_to_chunks(x, dimension)
-    Ys = [transform(x, A, b) for x in Xs]
-    return [num for vec in Ys for num in vec]  # flatten
-
-
 # TESTS!
 # append_one_and_stack
 assert np.all(append_one_and_stack([(1, 2), (3, 4)]) == \
               np.array([[1, 3], [2, 4], [1, 1]])
              )
-
-# list_to_chunks
-assert list_to_chunks([1, 2, 3, 4], 2) == [[1, 2], [3, 4]]
 
 # solve_affine_transformation and transform
 Ys = [(0, 0), (9.8, 0), (9.8, 6.3)]
@@ -61,10 +45,3 @@ for Xs in [a_Xs, b_Xs]:
     A, b = solve_affine_transformation(Xs, Ys)
     for x, y in zip(Xs, Ys):
         assert np.allclose(transform(x, A, b), y)
-
-# transform_all
-y = [num for vec in Ys for num in vec]  # flatten
-for Xs in [a_Xs, b_Xs]:
-    A, b = solve_affine_transformation(Xs, Ys)
-    x = [num for vec in Xs for num in vec]  # flatten
-    assert np.allclose(transform_all(x, A, b), y)
