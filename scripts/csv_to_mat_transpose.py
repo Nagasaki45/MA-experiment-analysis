a390804f60d1826afafebb289f45fb9cfa1534d4
Tom Gurion <nagasaki45@gmail.com>
2014-10-22 21:33:10 +0300

    Adding interaction_survey_q2_session_1_vs_2 analsis (cooked -> spss_ready -> spss_results)

--- scripts/csv_to_mat_transpose.py
+++ scripts/csv_to_mat_transpose.py
@@ -15,20 +15,21 @@ OUTPUT_DIR = os.path.join('..', 'spss_ready')
 
 
 def _group_row(original, group_name):
-    columns = ['group', 's1', 's2', 's3']
+    unique_session_values = np.unique(original['session'])
     g_data = original[original['group'] == group_name]
-    sessions = (g_data[g_data['session'] == s] for s in (1, 2, 3))
+    sessions = (g_data[g_data['session'] == s] for s in unique_session_values)
     s_values = (s.val.values for s in sessions)
     as_columns = (s[:, np.newaxis] for s in s_values)
     horizontal = np.hstack(as_columns)
+    columns = ['group'] + ['s{}'.format(i) for i in unique_session_values]
     new = pd.DataFrame(horizontal, columns=columns[1:])
     new['group'] = group_name
     return new[columns]
 
 
 def to_spss_mat(original):
-    new = pd.concat((_group_row(original, group) for group in ('A', 'B')),
-                    ignore_index=True)
+    rows = (_group_row(original, group) for group in ('A', 'B'))
+    new = pd.concat(rows, ignore_index=True)
     new.index.name = 'part'
     return new
 
@@ -38,6 +39,7 @@ def main():
         # dont even try to work on cooked video data
         if filename.startswith('video'):
             continue
+        print('Working on {}...'.format(filename))
         path = os.path.join(COOKED_DIR, filename)
         original = pd.read_csv(path, names=['group', 'session', 'val'])
         new = to_spss_mat(original)
